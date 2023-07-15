183d0a33bf2910e7ada319c4e0d6f417558e6462
DanielGnzlzVll <jodgonzalezvi@unal.edu.co>
2023-07-15 15:27:11 -0500

    fix default settings

--- asacx/asgi.py
+++ asacx/asgi.py
@@ -6,7 +6,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from core import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asacx.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
